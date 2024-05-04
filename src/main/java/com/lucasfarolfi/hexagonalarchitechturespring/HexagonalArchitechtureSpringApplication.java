a86ecfe85b962684149a47c0787fcf0bf59b1e60
lucasfarolfi <lucasfarolfi.rj@gmail.com>
2024-05-04 18:49:28 -0300

    init project

---
+++ src/main/java/com/lucasfarolfi/hexagonalarchitechturespring/HexagonalArchitechtureSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lucasfarolfi.hexagonalarchitechturespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitechtureSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchitechtureSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasfarolfi/hexagonalarchitechturespring/HexagonalArchitechtureSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasfarolfi.hexagonalarchitechturespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitechtureSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
